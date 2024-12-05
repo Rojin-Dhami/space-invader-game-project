d55cb5377d591aec4c4bb4c611898114b77b5a36
Rojin-Dhami <rojindhami98@gmail.com>
2024-12-05 21:37:49 +0545

    second commit

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 import pygame
-#initialize the pygame 
+
+# initialize the pygame 
 pygame.init()
-#create the screen
+
+# create the screen
 screen=pygame.display.set_mode((800,600))
-#Title and icon
+
+
+# Title and icon
 pygame.display.set_caption("SPACE INVADERS")
 icon=pygame.image.load("coolship.png")
 pygame.display.set_icon(icon)
-#game loop
 
+#PLAYER IMAGE 
+playerimg=pygame.image.load("warship.png")
+playerX=370
+playerY=400
+
+
+def player(x,y):
+   screen.blit(playerimg,(x,y))
+  
+
+
+# Game loop
 running=True
 while running:
-    for event in pygame.event.get():
+   #RGB
+   screen.fill((0,0,0))
+   for event in pygame.event.get():
          if event.type==pygame.QUIT:
             running=False
-         screen.fill((255,10,190))
+
+#INPUT CONTROL
+         if event.type==pygame.KEYUP:
+            if  event.type==pygame.K_RIGHT:
+               print("left key is pressed")
+            if event.type==pygame.K_RIGHT:
+               print("right key is pressed")
+             
+         elif event.type==pygame.KEYDOWN:
+            if event.type==pygame.K_RIGHT or event.type==pygame.K_RIGHT:
+               print("key is released")
+             
+         player(playerX,playerY)
          pygame.display.update()
+         
             
