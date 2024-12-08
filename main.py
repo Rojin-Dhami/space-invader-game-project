6bb9c226b5c3fd9da6a8f6c702afec06ae73c69e
Rojin-Dhami <rojindhami98@gmail.com>
2024-12-08 23:33:43 +0545

    third commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # initialize the pygame 
 pygame.init()
@@ -12,15 +13,30 @@ pygame.display.set_caption("SPACE INVADERS")
 icon=pygame.image.load("coolship.png")
 pygame.display.set_icon(icon)
 
+
+#BACKGROUND
+background=pygame.image.load("back.png")
+
 #PLAYER IMAGE 
 playerimg=pygame.image.load("warship.png")
 playerX=370
 playerY=400
+playerX_change=0
 
 
 def player(x,y):
    screen.blit(playerimg,(x,y))
   
+
+#Enemy IMAGE 
+enemyimg=pygame.image.load("space invaders.png")
+enemyX=random.randint(0,800)
+enemyY=random.randint(0,600)
+enemyX_change=0.25
+enemyY_change=20
+
+def enemy(x,y):
+   screen.blit(enemyimg,(x,y))
 
 
 # Game loop
@@ -28,22 +44,52 @@ running=True
 while running:
    #RGB
    screen.fill((0,0,0))
+   screen.blit(background,(0,0))
+   #EVENT LOOP
    for event in pygame.event.get():
-         if event.type==pygame.QUIT:
-            running=False
+      if event.type==pygame.QUIT:
+         running=False
 
 #INPUT CONTROL
-         if event.type==pygame.KEYUP:
-            if  event.type==pygame.K_RIGHT:
-               print("left key is pressed")
-            if event.type==pygame.K_RIGHT:
-               print("right key is pressed")
+      if event.type==pygame.KEYDOWN:
+      
+         if  event.key==pygame.K_RIGHT:
+            playerX_change=0.25
+         if event.key==pygame.K_LEFT:
+            playerX_change=-0.25
+               
              
-         elif event.type==pygame.KEYDOWN:
-            if event.type==pygame.K_RIGHT or event.type==pygame.K_RIGHT:
-               print("key is released")
-             
-         player(playerX,playerY)
-         pygame.display.update()
+      if event.type==pygame.KEYUP:
+            
+         if event.key==pygame.K_RIGHT or event.key==pygame.K_LEFT:
+            playerX_change=0
+   
+   #UPDATE PLAYER POSITION           
+   playerX+=playerX_change
+
+   #BOUNDARY CHECK
+   if playerX<=0:
+      playerX=0
+   elif playerX>=736:
+      playerX=736
+
+  #ENEMY MOVEMENT CONTROL
+   enemyX+=enemyX_change
+   if enemyX<=0:
+      enemyX_change=0.25
+      enemyY+=enemyY_change
+   elif enemyX>=736:
+      enemyX_change=-0.25
+      enemyY+=enemyY_change
+
+
+   #DRAWS PLAYER
+   player(playerX,playerY)
+
+   #DRAWS enemy
+   enemy(enemyX,enemyY)
+
+   #UPDATE DISPLAYY
+   pygame.display.update()
          
             
